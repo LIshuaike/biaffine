c03ef0f70610398c3fb716488d8eec08023812a1
skli <lsk1234@qq.com>
2020-09-28 11:18:54 +0800

    add parallel.py

--- dparser/modules/__init__.py
+++ dparser/modules/__init__.py
@@ -1,11 +1,12 @@
 from .biaffine import Biaffine
 from .bilstm import BiLSTM
+from .bert import BertEmbedding
 from .char_lstm import CHAR_LSTM
 from .dropout import IndependentDropout, SharedDropout
 from .mlp import MLP
 from .scalar_mix import ScalarMix
 
 __all__ = [
-    'CHAR_LSTM', 'MLP', 'Biaffine', 'BiLSTM', 'IndependentDropout',
-    'ScalarMix', 'SharedDropout'
+    'CHAR_LSTM', 'MLP', 'BertEmbedding', 'Biaffine', 'BiLSTM',
+    'IndependentDropout', 'ScalarMix', 'SharedDropout'
 ]
